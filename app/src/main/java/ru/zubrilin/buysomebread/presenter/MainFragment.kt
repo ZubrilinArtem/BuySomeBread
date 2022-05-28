f338b07198a8433e109428049a5fae451e8ada2c
Zubrilin Artem <zubrilinartem1994@gmail.com>
2022-05-28 12:48:06 +0300

    add presentation

---
+++ app/src/main/java/ru/zubrilin/buysomebread/presenter/MainFragment.kt
@@ -0,0 +1,20 @@
+package ru.zubrilin.buysomebread.presenter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.zubrilin.buysomebread.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
---
+++ app/src/main/java/ru/zubrilin/buysomebread/presenter/TaskFragment.kt
@@ -0,0 +1,20 @@
+package ru.zubrilin.buysomebread.presenter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.zubrilin.buysomebread.R
+
+class TaskFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_task, container, false)
+    }
+
+}
